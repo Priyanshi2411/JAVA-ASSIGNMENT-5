ca3e39e0389d73dc6268b6628ff9ba92a58935d5
Priyanshi2411 <110334446+Priyanshi2411@users.noreply.github.com>
2022-10-08 16:45:34 +0530

    Add files via upload

---
+++ Pr4.java
@@ -0,0 +1,40 @@
+//ID : 21CE141
+//Name : Priyanshi Talaviya 
+//Aim :  WAP to show use of character and byte stream.
+ import java.io.FileInputStream;
+ import java.io.FileOutputStream;
+ import java.io.IOException;
+
+public class Pr4 {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+
+        try {
+            fis = new FileInputStream("InputFile.jpg");
+            fos = new FileOutputStream("OutputFile.jpg");
+
+            int temp;
+
+            while ((temp = fis.read()) != -1) {
+                fos.write(temp);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }// Prepared by Priyanshi Talaviya 21CE141
